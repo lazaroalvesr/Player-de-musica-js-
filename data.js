0c5f4a6f5542bccefca7a8b4409e41a499ae7f2e
Lazaro Alves <lazaroalves12355@gmail.com>
2023-02-07 13:28:12 -0300

    first commit

---
+++ data.js
@@ -0,0 +1,20 @@
+let songs = [
+    {
+        name: 'Bonito Demais Pra Ser Verdade',
+        path: 'musica/Music(1).mp3',
+        artist: 'Blade H2',
+        cover: 'imagem/balde.jpeg'
+    },
+    {
+        name: 'Festa Na Favela',
+        path: 'musica/Music(2).mp3',
+        artist: 'Misael',
+        cover: 'imagem/misael.jpg'
+    },
+    {
+        name: '24 Horas',
+        path: 'musica/Music(3).mp3',
+        artist: 'WC NO BEAT, Hyperanhas & N.I.N.A ',
+        cover: 'imagem/wcnobet.jpg'
+    },
+]
